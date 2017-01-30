57df9c3e569dd6940227e3d746b5036a1d50213c
Sergey Noskov <snoskov@avito.ru>
2017-01-30 14:11:30 +0300

    Added time formatting option for clock watcher

--- src/watchers/clock.rs
+++ src/watchers/clock.rs
@@ -2,23 +2,36 @@ use chrono::Local;
 use config::MainConfig;
 use watcher::*;
 
+#[derive(Debug, RustcDecodable, Clone)]
+struct ClockWatcherConfig {
+    format: String,
+}
+
+impl Default for ClockWatcherConfig {
+    fn default() -> ClockWatcherConfig {
+        ClockWatcherConfig { format: "%v %T".to_string() }
+    }
+}
+
 
 #[derive(Default, Clone)]
 pub struct ClockWatcher {
     tag: String,
+    config: ClockWatcherConfig,
 }
 
 impl Watcher for ClockWatcher {
-    fn configure(&mut self, _: &MainConfig, tag: &str) {
+    fn configure(&mut self, main_conf: &MainConfig, tag: &str) {
+        self.config = main_conf.decode_tag(tag).unwrap();
         self.tag = tag.to_string();
     }
 
     fn into_action(self) -> Box<Fn() -> Tagged + Send> {
         let z = move || -> Tagged {
-            let now = Local::now();
+            let now = format!("{}", Local::now().format(&self.config.format));
             Tagged {
                 tag: self.tag.clone(),
-                data: now.to_rfc2822(),
+                data: now,
             }
         };
 
